2cfea25cbc5f7c371c0bb1c6085454d6aed02fc9
Laert Xhumari <laertxhumari@laerts-macbook-pro.local>
2019-02-23 15:01:17 -0500

    Initial Commit

---
+++ Assigment1Tests/Assigment1Tests.swift
@@ -0,0 +1,34 @@
+//
+//  Assigment1Tests.swift
+//  Assigment1Tests
+//
+//  Created by Laert Xhumari on 2/23/19.
+//  Copyright © 2019 Laert Xhumari. All rights reserved.
+//
+
+import XCTest
+@testable import Assigment1
+
+class Assigment1Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
